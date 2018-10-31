651b887ea89a916ea80fa3414f4ebbf20f10f032
EC2 Default User <ec2-user@ip-172-31-36-102.us-east-2.compute.internal>
2018-10-31 23:40:56 +0000

    add cucumber test for homepage signup and signin

--- features/sign_up.feature
+++ features/sign_up.feature
@@ -1,6 +1,36 @@
-Feature: Signup Action
+Feature: see the welcome page, sign up and sign in
+As a product manager
+I want our users to be greeted when they visit our site
+So that they have a better experience
+
+Scenario: User sees the welcome message
+When I go to the homepage
+Then I should see the welcome message
+
   
 Scenario: Signup with Valid Credentials
-    Given I am on Home Page
-    When I click the 'Sign up now!'
-    Then I should see the Signup page
+Given I go to the homepage
+When I click the 'sign up now'
+Then I should see the Signup page
+When I am a new, authenticated user
+# When I fill in "firstname" with "wenjie"
+# And I fill in "lastname" with "zhang"
+# And I fill in "email" with "test@test.com"
+# And I fill in "password" with "test123pass"
+# And I fill in "password_confirmation" with "test123pass"
+
+Scenario: Signin with Valid Credentials
+Given I go to the homepage
+When I enter the username and password
+
+
+
+
+
+# Scenario: login with valid credentials
+#   Given I am on the login page
+#   When I fill in "Email" with "test@test.com"
+#   And I fill in "Password" with "test123pass"
+#   And I press "Login"
+#   Then I should be on the users home page
+#   And I should see "Login successful"
--- features/step_definitions/sign_up_steps.rb
+++ features/step_definitions/sign_up_steps.rb
@@ -1,11 +1,51 @@
-Given("I am on Home Page") do
+# Given("I am on Home Page") do
+#   visit root_path
+# end
+
+# When("I click the 'Sign up now") do
+#   # touch("view marked:'Sign up now!'")
+#   pending
+# end
+
+When(/^I go to the homepage$/) do
   visit root_path
 end
 
-When("I click the 'Sign up now!'") do
-  touch("view marked:'Sign up now!'")
+Then(/^I should see the welcome message$/) do
+  expect(page).to have_content("Welcome to TamUber Driver Safety Interface")
 end
 
-Then("I should see the Signup page") do
-  pending # Write code here that turns the phrase above into concrete actions
+When(/^I click the 'sign up now'$/) do 
+  visit signup_path
 end
+  
+Then(/^I should see the Signup page$/) do
+  expect(page).to have_content("Sign up")
+end
+
+When(/^I am a new, authenticated user$/) do
+  lastname = "zhang"
+  firstname = "wenjie"
+  email = 'testing@man.net'
+  password = 'secretpass'
+  password_confirmation = 'secretpass'
+  User.new(:firstname => firstname, :lastname => lastname, :email => email, :password => password, :password_confirmation => password).save!
+end
+
+When(/^I enter the username and password$/) do 
+end 
+
+
+
+
+  # visit '/users/sign_in'
+  # fill_in "user_email", :with => email
+  # fill_in "user_password", :with => password
+  # click_button "Sign in"
+# And I fill in "lastname" with "zhang"
+# And I fill in "email" with "test@test.com"
+# And I fill in "password" with "test123pass"
+# And I fill in "password_confirmation" with "test123pass"
+# And I press "Create my account"
+# Then I should see the welcome message
+

2600cb9ede8ccbfc5c852c9772aca7df74bdbacd
Anjali Chadha <anjali.chadha229@gmail.com>
2018-11-17 01:59:31 +0000

    Fixing the structure of view || Dashbaord page bug fix

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class DashboardController < ApplicationController
 
-  def new
+  def diagnostics
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   get    '/try',   to: 'static_pages#try'
-  get    '/dashboard',  to: 'dashboard#new'
+  get    '/dashboard',  to: 'dashboard#diagnostics'
   resources :users
 end

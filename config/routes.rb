0cd57fbb49b1196b35ceea6422b7e9705db77e56
kexincui <kexincuichina@gmail.com>
2018-11-18 05:19:51 +0000

    (test) Testing Checklist functionality for app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root   'static_pages#home'
   get    '/help',    to: 'static_pages#help'
+  get    '/checklist',    to: 'static_pages#checklist'
   get    '/about',   to: 'static_pages#about'
   get    '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'

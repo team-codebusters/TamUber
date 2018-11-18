5766da742816976bb1fdc21072cf68e6f22d233d
kexincui <kexincuichina@gmail.com>
2018-11-18 07:19:07 +0000

    (test_kexin) Preventing proceeding to home page without having all boxes checked. Shows error message.

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -4,8 +4,17 @@ class TestController < ApplicationController
   end
 
   def create
-    flash.now[:danger] = params[:session]
-    redirect_to current_user
+    windshield =  params[:session][:check_windshield]
+    tire =  params[:session][:check_tire]
+    light =  params[:session][:check_light]
+    gas =  params[:session][:check_gas]
+    if windshield =="1" and tire =="1" and light=="1" and gas=="1"
+        redirect_to current_user
+    else
+        flash[:error] = 'Unsuccessful in checking in becuase not all the checkboxes were checked! Please Sign in and make sure that all the checkboxes have been checked!'
+        log_out if logged_in?
+        redirect_to root_url
+    end 
   end
 
   def destroy

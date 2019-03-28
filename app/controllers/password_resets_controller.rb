05407bb4033a38a164da4fcfb1b6ca8ad0eaa813
EC2 Default User <ec2-user@ip-172-31-35-12.us-east-2.compute.internal>
2019-03-28 20:01:16 +0000

    comment test

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -24,8 +24,8 @@ class PasswordResetsController < ApplicationController
   def create
     @user = User.find_by(email: params[:password_reset][:email].downcase)
     if @user
-      # @user.create_reset_digest
-      @user.send_password_reset_email
+      @user.create_reset_digest
+      # @user.send_password_reset_email
       flash[:info] = "Email sent with password reset instruction"
       redirect_to root_url
     else

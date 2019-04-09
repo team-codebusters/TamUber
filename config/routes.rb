0288263d24ce5693217ba6c37676e33fdea11ce0
EC2 Default User <ec2-user@ip-172-31-44-62.us-east-2.compute.internal>
2019-04-09 22:37:41 +0000

    Login in page changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # TO DO set the heroku url below
-   default_url_options :host => 'static_pages#home'
+   default_url_options :host => 'https://password111.herokuapp.com/'
   get 'password_resets/new'
 
   get 'password_resets/edit'

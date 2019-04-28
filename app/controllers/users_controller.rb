cac23e3d5a2e8c0a4f198d02d6211fccdd4e93f6
EC2 Default User <ec2-user@ip-172-31-93-252.ec2.internal>
2019-04-28 21:02:04 +0000

    asdad

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,11 +13,18 @@ class UsersController < ApplicationController
     source = 'http://47.218.218.78:8080/car_info.json'
     result = JSON.parse(Net::HTTP.get_response(URI.parse(source)).body)
     
+    now = Time.now
+    past_array = result['time'].split('_')
+    past = Time.new(past_array[0], past_array[1], past_array[2], past_array[3], past_array[4])
+    diff = now - past
+    @diff_time = "Days:%02d, Hours:%02d, Minutes:%02d, Seconds:%02d" % [diff/86400, diff / 3600 % 24, diff / 60 % 60, diff % 60]
+    
+
     @vehicle_stats = {"tire_pressure"=> result['tire pressure'], "battery_level"=>result['battery'], "lidar_status"=> result['lidar status'], "time"=> result['time']}
     
     
     
-    return @user,@vehicle_stats
+    return @user,@vehicle_stats,@diff_time
   end
 
   def new

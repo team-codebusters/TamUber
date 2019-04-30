b6f152986fb974592e1800a4174980b98bb2283e
TedTan <tedtan@tamu.edu>
2019-04-30 22:35:55 +0000

    login page full screen

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,20 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @vehicle_stats =  VehicleStatus.first()
+    #@vehicle_stats =  VehicleStatus.first()
+    @vehicle_stats = VehicleStatus.first()
+    require 'json'
+    file = File.read('car_info.json')
+    data_hash = JSON.parse(file)
+    time = data_hash['Time']
+    timearray = time.split('_')
+    past = Time.new(timearray[0],timearray[1],timearray[2],timearray[3],timearray[4],0,"-05:00")
+    timenow = Time.now
+    diff = timenow - past
+    @vehicle_stats = {"tire_pressure"=> data_hash['tire pressure'], 
+    "battery_level"=>data_hash['battery'], "lidar_status"=> 'Online', "time"=> diff/60}
     return @user,@vehicle_stats
+    #file = File.read('file-name-to-be-read.json')
   end
 
   def new

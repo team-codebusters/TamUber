af4df5390da67825f72c90a1267dd0c4c49c751b
EC2 Default User <ec2-user@ip-172-31-93-252.ec2.internal>
2019-04-30 18:45:31 +0000

    added timeout feature when server is down

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,17 +10,31 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     # @vehicle_stats =  VehicleStatus.first()
     
-    source = 'http://47.218.218.78:8080/car_info.json'
-    result = JSON.parse(Net::HTTP.get_response(URI.parse(source)).body)
+    url = URI.parse('http://47.218.218.78:8080/car_info.json')
+
+    begin 
+    	http = Net::HTTP.new(url.host, url.port)
+    	http.read_timeout = 3
+    	http.open_timeout = 3
+    	resp = http.get(url.path) 
+    	result = JSON.parse(resp.body)
+    	
+    	now = Time.now
+      past_array = result['time'].split('_')
+      past = Time.new(past_array[0], past_array[1], past_array[2], past_array[3], past_array[4], past_array[5])
+      diff = now - past
+      @diff_time = "Hour:%d Minute:%d Second:%d" % [diff/3600, diff / 60 % 60, diff % 60]
+      @vehicle_stats = {"tire_pressure"=> result['tire pressure'], "battery_level"=>result['battery'], "lidar_status"=> result['lidar status'], "time"=> result['time']}
     
-    now = Time.now
-    past_array = result['time'].split('_')
-    past = Time.new(past_array[0], past_array[1], past_array[2], past_array[3], past_array[4], past_array[5])
-    diff = now - past
-    @diff_time = "Hour:%d Minute:%d Second:%d" % [diff/3600, diff / 60 % 60, diff % 60]
+      rescue 
+      	@diff_time = "Server is Down!"
+        @vehicle_stats = {"tire_pressure"=> "Server is Down!", "battery_level"=>"Server is Down!", "lidar_status"=> "Server is Down!", "time"=>"Server is Down!"}
+    end 
     
 
-    @vehicle_stats = {"tire_pressure"=> result['tire pressure'], "battery_level"=>result['battery'], "lidar_status"=> result['lidar status'], "time"=> result['time']}
+      
+
+    
     return @user,@vehicle_stats,@diff_time
   end
 

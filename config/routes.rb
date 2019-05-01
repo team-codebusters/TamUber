e524bb1c20b1207251e05a45f4112b4885375cc7
EC2 Default User <ec2-user@ip-172-31-93-252.ec2.internal>
2019-05-01 19:07:34 +0000

    final

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # TO DO set the heroku url below
-  default_url_options :host => 'https://password-reset-ted.herokuapp.com/'
+  default_url_options :host => 'http://tamuber123.herokuapp.com/'
   get 'password_resets/new'
   get 'password_resets/edit'
   root   'static_pages#home'
